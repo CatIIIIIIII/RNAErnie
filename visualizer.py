3edf46ab32bace6c700a6c59c277e5ab15aa59ba
CatIIIIIIII <wangning.roci@gmail.com>
2023-07-17 19:59:53 +0800

    visualdl

---
+++ visualizer.py
@@ -0,0 +1,85 @@
+"""
+This module creates visualizers.
+
+Author: wangning(wangning.roci@gmail.com)
+Date  : 2022/12/30 15:57
+"""
+
+from collections import defaultdict
+
+import paddle.static
+from visualdl import LogWriter
+
+
+class Visualizer(object):
+    """ Wraps a visualdl for tasks use.
+    """
+
+    def __init__(self,
+                 log_dir,
+                 name=""):
+        """init visualdl writer
+
+        Args:
+            log_dir (str): output directory
+            name (str, optional): recording file name. Defaults to "".
+        """
+        self.writer = LogWriter(logdir=log_dir)
+        self.name = name
+
+        self.tag_step = defaultdict(int)
+
+    def update_hparams(self, args):
+        """Add hyper-parameters information.
+
+        Args:
+            args: Arg parser of task settings.
+
+        Returns:
+            None
+        """
+        args_dict = vars(args)
+        self.writer.add_hparams(
+            hparams_dict=args_dict,
+            metrics_list=['none']
+        )
+
+    def update_scalars(self, tag_value, step):
+        """Update multiple tags at one time.
+
+        Args:
+            tag_value: {tag: value} dict for multiple updates
+            step: update interval instead of absolute steps
+
+        Returns:
+            None
+        """
+        for tag, value in tag_value.items():
+            self.writer.add_scalar(tag=tag, step=self.tag_step[tag], value=value)
+            self.tag_step[tag] += step
+
+    def update_model(self, model, input_shapes):
+        """Add model structure in visualdl.
+
+        Args:
+            model: network to display
+            input_shapes: shapes of all inputs, list
+
+        Returns:
+            None
+        """
+        input_spec = []
+        for s in input_shapes:
+            input_spec.append(paddle.static.InputSpec(shape=s, dtype='int64'))
+        self.writer.add_graph(
+            model=model,
+            input_spec=input_spec
+        )
+
+    def get_name(self):
+        """Get descriptor name of visualizer.
+
+        Returns:
+            instance name
+        """
+        return self.name
