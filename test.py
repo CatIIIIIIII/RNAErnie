4e10d0314ad9328f0ea74aa42312862500296664
WANG Ning <wangning.roci@gmail.com>
2024-06-25 21:05:23 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from rna_ernie import BatchConverter
 from paddlenlp.transformers import ErnieModel
 
 # ========== Set device
-paddle.set_device("gpu")
+paddle.set_device("cpu")
 
 # ========== Prepare Data
 data = [
@@ -15,12 +15,14 @@ data = [
 
 # ========== Batch Converter
 batch_converter = BatchConverter(k_mer=1,
-                                  vocab_path="./data/vocab/vocab_1MER.txt",
-                                  batch_size=256,
-                                  max_seq_len=512)
+                                 vocab_path="./data/vocab/vocab_1MER.txt",
+                                 batch_size=256,
+                                 max_seq_len=512)
 
 # ========== RNAErnie Model
-rna_ernie = ErnieModel.from_pretrained("output/BERT,ERNIE,MOTIF,PROMPT/checkpoint_final/")
+rna_ernie = ErnieModel.from_pretrained(
+    "output/BERT,ERNIE,MOTIF,PROMPT/checkpoint_final/")
+rna_ernie.eval()
 
 # call batch_converter to convert sequences to batch inputs
 for names, _, inputs_ids in batch_converter(data):
@@ -28,4 +30,5 @@ for names, _, inputs_ids in batch_converter(data):
         # extract whole sequence embeddings
         embeddings = rna_ernie(inputs_ids)[0].detach()
         # extract [CLS] token embedding
-        embeddings_cls = embeddings[:, 0, :]
+        embeddings_cls = embeddings[:, 0, :20]
+        print(embeddings_cls)
